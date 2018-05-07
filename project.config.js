8f4e8fcda5248e72b56cfdb5d202179d4ab1017d
Charlie Jackson <charlie@charliejackson.com>
2018-05-07 10:49:35 +0100

    Removed entry component as routes takes over

--- project.config.js
+++ project.config.js
@@ -1,10 +1,8 @@
-import Entry from "./src";
 import reducers from "./src/store/reducers";
 import routes from "./src/config/routes";
 
 export default {
   type: "web",
-  entryComponent: Entry,
   reducers,
   routes
 };
