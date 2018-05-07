9d61eafbea2a21316ad32faaa25ae23de5332d67
Charlie Jackson <charlie@charliejackson.com>
2018-05-07 10:02:00 +0100

    Render a button from lib

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import React from "react";
-import _ from "lodash";
-import test from "./test";
-
-console.log("Hi there");
-
-console.log(_.partition([1, 2, 3, 4], n => n % 2));
-
-const Component = () => <p>Hello there 2</p>;
-
-export default Component;
+export { default } from "./scenes/Start";
---
+++ src/scenes/Start/Start.render.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ButtonsText from "@cajacko/lib/src/components/Buttons/Text";
+
+const Start = () => (
+  <div>
+    <ButtonsText text="Hi" />
+  </div>
+);
+
+export default Start;
